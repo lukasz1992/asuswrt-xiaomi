03ac360e9dda253f92d54dbcf3c85316bfb40b21
Your Name <you@example.com>
2021-05-19 18:22:50 +0200

    Update some prebuilt binaries

--- release/src/router/rom/apps_scripts/getrealip.sh
+++ release/src/router/rom/apps_scripts/getrealip.sh
@@ -37,7 +37,7 @@ if [ "$(nvram get wans_mode)" = "lb" ] ; then
 		nvram set ${prefix}realip_ip=$result
 
 		wan=`echo $prefix|sed -e "s,_,,"`
-		[ -z "$result" ] && echo "$wan failed." || echo "$wan external IP is $result."
+		[ -z "$result" ] && echo "$wan failed." || echo "$wan external IP is $result"
 	done
 else
 	for prefix in $prefixes; do
